b976f6e3d9507da838837d06dbbd64b2b586425b
Lucas Peng <lucaspeng@Lucass-MacBook-Air.local>
2016-05-02 13:37:09 -0700

    gradient

--- Fields/Fields_gradient.jl
+++ Fields/Fields_gradient.jl
@@ -1,4 +1,16 @@
 #1.use cubic spline interpolation
 #2.use bicubic interpolation 4x4 pixels
-function value{N}(f::ScalarFieldNode,t::Real)
+function sample{T<:ComplexOrFLoat,N}(f::ScalarField{T,N},pos::Vector{Float64},t::Real,order::Integer = 3)
+    fpos = collect(f.position)
+    fres = collect(f.res)
+    fsize = collect(f.size)
+    pidx = div(pos/fres)+1
+    
+#    return T(f.field*f.scaling(t),f.position,f.size,scaling = t->1.0)
+end
+
+
+function sample{N}(f::ScalarFieldNode{N},pos::Vector{Float64},t::Real;order::Integer = 3)
+    # get (order+1)x(order+1) pixels of local field
+    
 end
