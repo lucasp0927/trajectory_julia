db0030b0ccc83665e850b562cb18ff01afec7700
Your Name <lucasp0927@gmail.com>
2016-05-02 09:45:30 -0700

    add gradient

--- Fields/Fields_function.jl
+++ Fields/Fields_function.jl
@@ -4,6 +4,7 @@ include("Fields_typeof.jl")
 include("Fields_align.jl")
 include("Fields_composite.jl")
 include("Fields_utility.jl")
+include("Fields_gradient.jl")
 # function initialize(res::Tuple{Integer,Integer}, sz::Tuple{Real,Real};dim = 2)
 #     global fields
 #     fields = ScalarFieldNode{dim}(Vector{Field}())
---
+++ Fields/Fields_gradient.jl
@@ -0,0 +1,4 @@
+#1.use cubic spline interpolation
+#2.use bicubic interpolation 4x4 pixels
+function value{N}(f::ScalarFieldNode,t::Real)
+end
