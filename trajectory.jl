2e1a324ddd6bafca7344793eb73c89c0b0b838c4
Lucas Peng <lucasp0927@gmail.com>
2016-09-13 15:22:19 -0700

    specify process number in commandline argument.

--- trajectory.jl
+++ trajectory.jl
@@ -1,4 +1,8 @@
-addprocs(4, exeflags="--depwarn=no")
+include("parse.jl")
+parsed_args = parse_commandline()
+println("Starting ",parse(Int,parsed_args["procs"])," processes.")
+addprocs(parse(Int,parsed_args["procs"]), exeflags="--depwarn=no")
+
 push!(LOAD_PATH, "./Fields")
 push!(LOAD_PATH, "./TrajSolver")
 push!(LOAD_PATH, "./TrajAnalyzer")
@@ -7,11 +11,10 @@ using TrajSolver
 using TrajAnalyzer
 @everywhere using Lumberjack
 include("fileio.jl")
-include("parse.jl")
 include("job_manage.jl")
 
 function prepare()
-    parsed_args = parse_commandline()
+#    parsed_args = parse_commandline()
     config_file,output_file = parsed_args["config"],parsed_args["outfile"]
     fields_config,trajsolver_config,job_config = parse_config(config_file)
     TrajSolver.init_parallel(trajsolver_config)
