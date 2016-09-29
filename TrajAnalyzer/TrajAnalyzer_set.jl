d25bbbc488e67aa872c2a02e1da48a11d539efe6
Lucas Peng <lucasp0927@gmail.com>
2016-09-29 00:17:54 +0000

    add trajectory filter

--- TrajAnalyzer/TrajAnalyzer_set.jl
+++ TrajAnalyzer/TrajAnalyzer_set.jl
@@ -1,4 +1,6 @@
 function init_parallel!(result::Dict,probe_sfn::ScalarFieldNode,ForceFields_sfn::ScalarFieldNode,config::Dict)
+    #filter trajectories
+    filter_traj(result,config["filter"])
     traj_s = copy_to_sharedarray!(result["traj"])
     result_wo_traj = result
     delete!(result_wo_traj,"traj")
@@ -32,5 +34,28 @@ function init!(result::Dict,traj_s::SharedArray{Float64},probe_sfn::ScalarFieldN
     elseif TA_Config["type"] == "double-scan-scaling"
         range_i = collect(Int(TA_Config["range_i_start"]):Int(TA_Config["range_i_end"]))
         range_j = collect(Int(TA_Config["range_j_start"]):Int(TA_Config["range_j_end"]))
+    end
+end
+"""
+    filter:
+        type: crashed
+        tstart: 1350.0
+        tend: 1420.0
+        gap: [9890.0, 10110.0, 10110.0, 9890.0, 25120.0, 25120.0, 24880.0, 24880.0]
+"""
+function filter_traj(result::Dict,config::Dict)
+    if config["type"] == "none"
+        info("Not filtering trajectories.")
+    elseif config["type"] == "crashed"
+        info("Select crashed trajectories.")
+        start_idx = searchsortedlast(result["tspan"],config["tstart"])
+        end_idx = searchsortedlast(result["tspan"],config["tend"])
+        traj = result["traj"]
+        selected = filter(i->any(isnan(traj[:,start_idx:end_idx,i])),1:size(traj,3))
+        info("select $(length(selected)) trajectories from $(size(traj,3)) trajectories.")
+        result["traj"] = cat(3,map(i->traj[:,:,i],selected)...)
+    elseif config["type"] == "gap"
+        info("Select trajectories pass through the gap.")
+        pp = Polygon([promote(config["gap"]...)...])
     end
 end
