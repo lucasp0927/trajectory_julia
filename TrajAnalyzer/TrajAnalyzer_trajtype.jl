b1c97974d1d0be3e47560477b3f000f558036938
Lucas Peng <lucasp0927@gmail.com>
2016-10-12 01:37:07 +0000

    change non-gap filter function.

--- TrajAnalyzer/TrajAnalyzer_trajtype.jl
+++ TrajAnalyzer/TrajAnalyzer_trajtype.jl
@@ -68,5 +68,20 @@ function filter_traj(result::Dict,config::Dict)
         selected = filter(i->anyPointInPolygon(gap_p,traj[1:2,start_idx:end_idx,i]), selected)
         info("selected $(length(selected)) trajectories from $(size(traj,3)) trajectories.")
         result["traj"] = cat(3,map(i->traj[:,:,i],selected)...)
+    elseif config["type"] == "non-gap"
+        info("Select trajectories dont pass through the gap.")
+        start_idx = searchsortedlast(result["tspan"],config["tstart"])
+        end_idx = searchsortedlast(result["tspan"],config["tend"])
+        traj = result["traj"]
+        gap_p = Polygon([promote(config["gap"]...)...])
+        roi_p = Polygon([promote(config["roi"]...)...])
+        selected = collect(1:size(traj,3))
+#        selected = filter(i->anyPointInPolygon(roi_p,traj[1:2,start_idx:end_idx,i]),selected)
+        #if exclude_crashed
+        #    selected = filter(i->any(isnan(traj[:,start_idx:end_idx,i]))==false,selected)
+        #end
+        selected = filter(i->anyPointInPolygon(gap_p,traj[1:2,start_idx:end_idx,i])==false, selected)
+        info("selected $(length(selected)) trajectories from $(size(traj,3)) trajectories.")
+        result["traj"] = cat(3,map(i->traj[:,:,i],selected)...)
     end
 end
