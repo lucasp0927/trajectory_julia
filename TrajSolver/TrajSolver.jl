01fcf09a862de6f53c28a151fd27acabf918e9da
Lucas Peng <lucasp0927@gmail.com>
2019-01-24 13:34:09 -0800

    remove dead code.

--- TrajSolver/TrajSolver.jl
+++ TrajSolver/TrajSolver.jl
@@ -50,15 +50,13 @@ function calculate_traj()
     #preallocate result according to dim
     if sim_type == "2D"
         traj = Array{Float64}(undef,4,length(tspan),trajnum)
+        @info "calculate trajectories..."
+        @time calculate_traj_inner_parallel_2d(init_xv,traj)
     else
         traj = Array{Float64}(undef,6,length(tspan),trajnum)
+        @info "calculate trajectories..."
+        @time calculate_traj_inner_parallel_3d(init_xv,traj)
     end
-    # function to produce the next work item from the queue.
-    # in this case it's just an index.
-    @info "calculate trajectories..."
-
-    @time calculate_traj_inner_parallel(init_xv,traj)
-
     #truncate save time range
     t_idx_start = searchsortedlast(tspan,trajsolver_config["save-range"]["tstart"])
     t_idx_end = searchsortedfirst(tspan,trajsolver_config["save-range"]["tend"])
@@ -69,9 +67,6 @@ function calculate_traj()
                   "tspan"=>tspan_save,
                   "pos"=>Fields.fields.position,
                   "siz"=>Fields.fields.size,
-#                  "radial_temperature"=>radial_temperature,
-#                  "axial_temperature"=>axial_temperature,
-#                  "init_speed"=>init_speed,
                   "reltol"=>reltol,
                   "abstol"=>abstol
                   )
@@ -79,7 +74,7 @@ function calculate_traj()
     return result
 end
 
-function calculate_traj_inner_parallel(init_xv,traj)
+function calculate_traj_inner_parallel_2d(init_xv,traj)
     i = 1
     pm = Progress(trajnum,1)
     @sync begin
@@ -92,53 +87,26 @@ function calculate_traj_inner_parallel(init_xv,traj)
                     if idx>trajnum
                         break
                     end
-                    traj[:,:,idx] = remotecall_fetch(solve_traj_one_shot,p,init_xv[:,idx])
+                    traj[:,:,idx] = remotecall_fetch(solve_traj_one_shot_2d,p,init_xv[:,idx])
                 end
             end
         end
     end
 end
 
-function calculate_traj_inner(init_xv,traj)
-    i = 1
-    pm = Progress(trajnum,1)
-    for i = 1:trajnum
-        next!(pm)
-        if i>trajnum
-            break
-        end
-        traj[:,:,i] = solve_traj_one_shot(init_xv[:,i])
-    end
-end
+# function calculate_traj_inner(init_xv,traj)
+#     i = 1
+#     pm = Progress(trajnum,1)
+#     for i = 1:trajnum
+#         next!(pm)
+#         if i>trajnum
+#             break
+#         end
+#         traj[:,:,i] = solve_traj_one_shot(init_xv[:,i])
+#     end
+# end
 
-function calculate_traj_unbalanced()
-#    parallel_set_iter(i)
-    prepare_U_prob()
-    @time @sync begin
-        for p = 2:nprocs()
-            @async remotecall_wait(solve_traj,p)
-        end
-    end
-    temp = cell(nworkers())
-    for p = 2:nprocs()
-        temp[p-1] = remotecall_fetch(get_result,p)
-    end
-    traj = cat(3,temp...)
-    result = Dict(
-                  "traj"=>traj,
-                  "tspan"=>tspan,
-                  "pos"=>Fields.fields.position,
-                  "siz"=>Fields.fields.size,
-                  "radial_temperature"=>radial_temperature,
-                  "axial_temperature"=>axial_temperature,
-                  "init_speed"=>init_speed,
-                  "reltol"=>reltol,
-                  "abstol"=>abstol
-                  )
-    return result
-end
-
-function solve_traj_one_shot(init_xv::Vector{Float64})
+function solve_traj_one_shot_2d(init_xv::Vector{Float64})
     if sim_type::String == "2D"
         yout = Array{Float64}(undef,4,length(tspan))
     elseif sim_type::String == "3D"
@@ -147,12 +115,12 @@ function solve_traj_one_shot(init_xv::Vector{Float64})
     fill!(yout,NaN)
     if any(isnan.(init_xv)) == false
         #mycvode(Fields.gradient!,init_xv,tspan,yout;reltol=reltol, abstol=abstol)
-        solve_eq_of_motion(Fields.gradient_odejl!,init_xv,tspan,yout;reltol=reltol, abstol=abstol)
+        solve_eq_of_motion_2d(Fields.gradient_odejl!,init_xv,tspan,yout;reltol=reltol, abstol=abstol)
     end
     return yout
 end
 
-function solve_eq_of_motion(f::Function, y0::Vector{Float64}, t::Vector{Float64} , yout::T; reltol::Float64=1e-8, abstol::Float64=1e-7, mxstep::Int64=Integer(1e6)) where T<:AbstractArray
+function solve_eq_of_motion_2d(f::Function, y0::Vector{Float64}, t::Vector{Float64} , yout::T; reltol::Float64=1e-8, abstol::Float64=1e-7, mxstep::Int64=Integer(1e6)) where T<:AbstractArray
     #TODO: add more solver options
     if solver == "ADAMS"
         solver_alg = CVODE_Adams()
@@ -166,36 +134,13 @@ function solve_eq_of_motion(f::Function, y0::Vector{Float64}, t::Vector{Float64}
         dt = t[i] - t[i-1]
         step!(integrator,dt,true)
         yout[:,i] = integrator.u
-        if boundary(yout[1:2,i]) == false
+        if boundary_2d(yout[1:2,i]) == false
             break
         end
     end
 end
-#=
-function solve_traj()
-    fill!(result,NaN)
-    init_xv = distribute_atoms()
-    #initialize sundials
-    if solver == "ADAMS"
-        @debug "Using solver ADAMS"
-        mem = convert(Sundials.CVODE_ptr,Sundials.CVodeHandle(Sundials.CV_ADAMS, Sundials.CV_FUNCTIONAL))
-    elseif solver == "BDF"
-        @debug "Using solver BDF"
-        mem = convert(Sundials.CVODE_ptr,Sundials.CVodeHandle(Sundials.CV_BDF, Sundials.CV_NEWTON))
-    else
-        err("Unknown ODE solver $solver.")
-    end
-    init = zeros(Float64,4)
-    for i = 1:my_trajnum::Int64
-        init[:] = init_xv[:,i]
-        yout = slice(result::Array{Float64,3},:,:,i)
-        mycvode(mem,Fields.gradient!,init,tspan,yout; reltol = reltol, abstol =abstol)
-    end
-    #TODO: figure out how to delete mem
-    GC.gc()
-end
-=#
-@inbounds function boundary(pos::Vector{Float64})
+
+@inbounds function boundary_2d(pos::Vector{Float64})
     #return false if particle should be removed
     for p in in_boundaries::Vector{Polygon}
         if pointInPolygon(p,pos)==true
@@ -209,56 +154,4 @@ end
     end
     return true
 end
-
-@inbounds function mycvode(f::Function, y0::Vector{Float64}, t::Vector{Float64} , yout::T; reltol::Float64=1e-8, abstol::Float64=1e-7, mxstep::Int64=Integer(1e6), userdata::Any=nothing) where T<:AbstractArray
-    # f, Function to be optimized of the form f(y::Vector{Float64}, fy::Vector{Float64}, t::Float64)
-    #    where `y` is the input vector, and `fy` is the
-    # y0, Vector of initial values
-    # t, Vector of time values at which to record integration results
-    # reltol, Relative Tolerance to be used (default=1e-4)
-    # abstol, Absolute Tolerance to be used (default=1e-6)
-    if solver == "BDF"
-        mem = Sundials.CVodeCreate(Sundials.CV_BDF, Sundials.CV_NEWTON)
-    elseif solver == "ADAMS"
-        mem = Sundials.CVodeCreate(Sundials.CV_ADAMS, Sundials.CV_FUNCTIONAL)
-    end
-    if mem == C_NULL
-        err("Failed to allocate CVODE solver object")
-    end
-
-    try
-        userfun = Sundials.UserFunctionAndData(f, userdata)
-        y0nv = Sundials.NVector(y0)
-        #flag = Sundials.CVodeInit(mem, cfunction(Sundials.cvodefun, Int32, (Sundials.realtype, Sundials.N_Vector, Sundials.N_Vector, Ref{Function})), t[1], Sundials.nvector(y0).ptr[1])
-        flag = Sundials.CVodeInit(mem, cfunction(Sundials.cvodefun, Cint, (Sundials.realtype, Sundials.N_Vector, Sundials.N_Vector, Ref{typeof(userfun)})), t[1], convert(Sundials.N_Vector, y0nv))
-        flag = Sundials.CVodeSetUserData(mem, userfun)
-        flag = Sundials.CVodeSetMaxNumSteps(mem,1e7)
-        flag = Sundials.CVodeSStolerances(mem, reltol, abstol)
-        flag = Sundials.CVDense(mem, length(y0))
-#        flag = Sundials.CVodeSetUserData(mem, f)
-#        flag = Sundials.CVodeSStolerances(mem, reltol, abstol)
-#        flag = Sundials.CVDense(mem, length(y0))
-        yout[1:2,1] = y0[1:2]
-        #yout[3,1] = Fields.value(y0[1:2],t[1])
-        yout[3:4,1] = y0[3:4]
-        ynv = Sundials.NVector(copy(y0))
-        tout = [t[1]]
-        for k in 2:length(t)
-            flag = Sundials.CVode(mem, t[k], ynv, tout, Sundials.CV_NORMAL)
-            #yout[3:4] are free spaces.
-            #yout[1:2,k] = y[1:2]
-            #yout[3:4,k] = y[3:4]
-            yout[:,k] = convert(Vector,ynv)
-            #        yout[3,k] = Fields.value(y[1:2],t[k])
-            #        yout[4,k] = t[k]
-
-            if boundary(yout[1:2,k]) == false
-                break
-            end
-        end
-    finally
-        Sundials.CVodeFree(Ref{Sundials.CVODEMemPtr}(mem))
-    end
-end
-
 end
